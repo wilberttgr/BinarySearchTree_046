49db0898aa0bfa28d15ad549ce23fa51f6fa543f
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-29 17:33:49 +0700

    add menu and case 1

--- BinarySearchTree_046/Program.cs
+++ BinarySearchTree_046/Program.cs
@@ -99,7 +99,7 @@ namespace BinarySearchTree_046
         }
         public void postorder(Node ptr) // performs the postorder traversal of the tree
         {
-            if  (ROOT == null)
+            if (ROOT == null)
             {
                 Console.WriteLine("Tree is empty");
                 return;
@@ -111,11 +111,31 @@ namespace BinarySearchTree_046
                 Console.WriteLine(ptr.info + " ");
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nEnter your choice (1-5) : ");
+                char ch = Convert.ToChar(Console.ReadKey());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                }
+            }
         }
     }
 }
