1764296ab702bbd5377634603f68382dca567e44
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-29 17:03:07 +0700

    add class node and constructor node class

--- BinarySearchTree_046/Program.cs
+++ BinarySearchTree_046/Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_046
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the NOde Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
