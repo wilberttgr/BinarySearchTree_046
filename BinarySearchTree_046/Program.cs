cce7f6e2ddb05f50fa68d7b15ce86da84324b3c0
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-29 17:17:14 +0700

    add method search

--- BinarySearchTree_046/Program.cs
+++ BinarySearchTree_046/Program.cs
@@ -55,6 +55,21 @@ namespace BinarySearchTree_046
                 }
             }
         }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            //This function searchs the currentNode of the specified Node as well as the current Node of parent
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
