233f04500b93d7b69d873d83e89b21559d3c73e6
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-29 17:19:13 +0700

    add method inorder

--- BinarySearchTree_046/Program.cs
+++ BinarySearchTree_046/Program.cs
@@ -69,6 +69,20 @@ namespace BinarySearchTree_046
                     currentNode = currentNode.rightchild;
             }
         }
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + " ");
+                inorder(ptr.rightchild);
+            }
+        }
     }
     internal class Program
     {
