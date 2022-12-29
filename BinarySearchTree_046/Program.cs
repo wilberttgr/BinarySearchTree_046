9873a845f46c3d05657315077510a095534b3779
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-29 17:07:54 +0700

    add class Binary Tree

--- BinarySearchTree_046/Program.cs
+++ BinarySearchTree_046/Program.cs
@@ -20,6 +20,16 @@ namespace BinarySearchTree_046
             rightchild = r;
         }
     }
+    /* A node class consist of three things, the information, references to the right child, and references to the left child. */
+
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; //initializing ROOT to null
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
